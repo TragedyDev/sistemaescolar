4b7b918c8650dfca3f7a41e747cb1b88a50ff880
Enrique <enriquejose10hidalgo@gmail.com>
2025-04-05 14:54:38 -0400

    first commit

---
+++ database/db.sql
@@ -0,0 +1,33 @@
+CREATE TABLE usuarios (
+    id_usuario INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nombres    VARCHAR (255) NOT NULL,
+    cargo      VARCHAR (255) NOT NULL,
+    email      VARCHAR (255) NOT NULL UNIQUE KEY,
+    password   TEXT NOT NULL,
+
+    fyh_creacion   DATETIME NULL,
+    fyh_actualizacion DATETIME NULL,
+    estado        VARCHAR (11)
+
+
+)ENGINE=InnoDB;
+INSERT INTO usuarios (nombres,cargo,email,password,fyh_creacion,estado)
+VALUES ('Freddy Hilari Michua','1','admin@admin.com','$2y$10$0tYmdHU9uGCIxY1f90W1EuIm54NQ8axowkxL1WzLbqO2LdNa8m3l2','2023-12-28 20:29:10','1');
+
+
+CREATE TABLE roles (
+
+  id_rol        INT (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  nombre_rol    VARCHAR (255) NOT NULL UNIQUE KEY,
+
+  fyh_creacion   DATETIME NULL,
+  fyh_actualizacion DATETIME NULL,
+  estado        VARCHAR (11)
+
+
+)ENGINE=InnoDB;
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('ADMINISTRADOR','2024-01-03 16:20:20','1');
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('DIRECTOR ACADÉMICO','2024-01-03 16:20:20','1');
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('DIRECTOR ADMINISTRATIVO','2024-01-03 16:20:20','1');
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('CONTADOR','2024-01-03 16:20:20','1');
+INSERT INTO roles (nombre_rol,fyh_creacion,estado) VALUES  ('SECRETARIA','2024-01-03 16:20:20','1');
---
+++ login/index.php
@@ -0,0 +1,92 @@
+<?php
+include ('../app/config.php');
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?=APP_NAME;?></title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?=APP_URL;?>/public/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?=APP_URL;?>/public/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?=APP_URL;?>/public/dist/css/adminlte.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+    <center>
+    <img src="https://img.freepik.com/vector-gratis/ilustracion-concepto-inicio-sesion_114360-739.jpg?t=st=1742674114~exp=1742677714~hmac=d025b8406a76ffe89113e5c0253f7befb2b9b40271ff5129cc308705c21833e6&w=740"
+    width= "150px" alt=""><br><br>
+    </center>
+  <div class="login-logo">
+    <h3 href=><b><?=APP_NAME;?></b></h3>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Inicio de Sesión</p>
+
+      <form action="controller_login.php" method="post">
+        <div class="input-group mb-3">
+          <input type="email" name="email" class="form-control" placeholder="Email">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" name= "password" class="form-control" placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <hr>
+        <div class="input-group mb-3">
+          <button class="btn btn-success btn-block" type="submit">Ingresar</button>
+        </div>
+      </form>
+
+      <?php
+      session_start();
+      if(isset($_SESSION['mensaje'])){
+          $mensaje = $_SESSION['mensaje'];
+          ?>
+          <script>
+              Swal.fire({
+                  position: "mid",
+                  icon: "error",
+                  title: "<?=$mensaje;?>",
+                  showConfirmButton: false,
+                  timer: 4000
+              });
+          </script>
+           <?php
+                session_destroy();
+            }
+            ?>
+
+
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="<?=APP_URL;?>/public/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?=APP_URL;?>/public/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?=APP_URL;?>/public/dist/js/adminlte.min.js"></script>
+</body>
+</html>
